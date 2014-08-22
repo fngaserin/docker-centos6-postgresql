7d32c4801a09b39a1199232821b3127b440480e5
Felix Ngaserin <felix@nusapro.com>
2014-08-22 11:15:04 +0700

    Update when configuration file is not found, a new database will be initialized on launch.

---
+++ postgres-launch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PGDATA=/var/lib/pgsql/9.3/data
+CONFIG_FILE_PATH=$PGDATA/postgresql.conf
+
+
+if [ ! -f $CONFIG_FILE_PATH ]; then
+
+su - postgres -c '/usr/pgsql-9.3/bin/initdb --locale=en_US.UTF-8'
+su - postgres -c 'pgtune -T Web -i $PGDATA/postgresql.conf > $PGDATA/local.conf'
+su - postgres -c 'echo "listen_addresses='"'"'*'"'"'" >> $PGDATA/local.conf'
+su - postgres -c 'echo "include = '"'"'local.conf'"'"'" >> $PGDATA/postgresql.conf'
+su - postgres -c 'echo "local all all trust" > $PGDATA/pg_hba.conf'
+su - postgres -c 'echo "host all all 127.0.0.1/32 trust" >> $PGDATA/pg_hba.conf'
+su - postgres -c 'echo "host all all 0.0.0.0/0 trust" >> $PGDATA/pg_hba.conf'
+su - postgres -c 'echo "fsync=off" >> $PGDATA/local.conf'
+
+cat $PGDATA/postgresql.conf
+fi
+
+su - postgres -c '/usr/pgsql-9.3/bin/postgres -D $PGDATA'
